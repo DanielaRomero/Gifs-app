e427650b55ab8be960b5878e455a648c98043ac5
DanielaRomero <daniela.romero@epn.edu.ec>
2024-01-28 18:13:02 -0500

    Gifs design

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './pages/home/home-page.component';
+import { SearchBoxComponent } from './componentes/search-box/search-box.component';
+import { CardListComponent } from './componentes/card-list/card-list.components';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent,
+    SearchBoxComponent,
+    CardListComponent,
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HomePageComponent
   ]
 })
 export class GifsModule { }
