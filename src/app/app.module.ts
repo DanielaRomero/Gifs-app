97e0046c74ba8872e6a7d6554b6ccd88e671a22f
DanielaRomero <daniela.romero@epn.edu.ec>
2024-01-28 22:09:40 -0500

    Gif API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
@@ -11,10 +13,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
     GifsModule,
-    SharedModule
+    HttpClientModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,25 +1,74 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 // ProvideIn: root, permite que todos los servicios esten disponibles para todo el aplicativo
 // cuando no esta en root toca imporrar el servicio a cada modulo
 
+
 @Injectable({providedIn: 'root'})
 export class GifsService {
 
-  private _tagsHistory: string[] = [];
+  public gifList: Gif[] = [];
+  private _tagsHistory:   string[] = [];
+  private apiKey:         string = '5VSRJK25YMnhIsM7GpYF2Mx00ihFKo37';
+  private serviceUrl:     string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() { }
+
+  constructor( private http: HttpClient) { }
+
 
   get tagsHistory(){
     // Crea una copia de los valores
     return [...this._tagsHistory];
   }
 
-  searchTag( tag: string): void{
+  private organizeHistory( tag : string ): void{
+    // Transforma a minuscula la palabra dado que TypeScript es case Sensitive
+    tag = tag.toLowerCase();
 
+    // Compara la nueva palabra buscada con la lista de palabras
+    if( this._tagsHistory.includes(tag) ){
+
+      // Si la pabra existe se filtra el arreglo y lo devuelve sin la palabra buscada
+      this._tagsHistory = this._tagsHistory.filter((oldTag)=> oldTag != tag );
+
+    }
+
+    // Se coloca la palabra al inicio
     this._tagsHistory.unshift(tag);
 
-    // console.log(this._tagsHistory);
+    // Se corta el arreglo para que solo tenga 10 elementos
+    this._tagsHistory = this._tagsHistory.splice(0,10);
+  }
+
+  async searchTag( tag: string): Promise<void>{
+
+    // Si no existe una palabra al poner enter no ingresa un elemento vacio al arreglo
+    if( tag.length === 0 ) return;
+
+    this.organizeHistory(tag);
+
+    const params = new HttpParams()
+    .set('api_key', this.apiKey)
+    .set('limit', '10')
+    .set('q', tag)
+
+    // Esto no es una promesa es un observable, es decir, a lo largo del tiempo
+    // puede emitir valores
+    // En este caso se subscribe para poder escuchar la respuesta
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+    .subscribe( resp =>{
+
+      this.gifList = resp.data;
+      console.log({gifs : this.gifList});
+
+    })
+
+    // Una forma de utilizar el api - utilizando promesas
+    // fetch('https://api.giphy.com/v1/gifs/search?api_key=5VSRJK25YMnhIsM7GpYF2Mx00ihFKo37&q=Valorant&limit=10')
+    // .then( resp => resp.json())
+    // .then(data => console.log(data))
   }
 
 }
